a1dff73b3c4eb46cdc73c52f93ed1ee4d2f67035
Muhammad Ali <ma496@outlook.com>
2025-06-16 12:53:41 +0500

    add data grid

--- ProcessesMonitor/ViewModels/MainWindowViewModel.cs
+++ ProcessesMonitor/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,34 @@
-﻿namespace ProcessesMonitor.ViewModels;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ProcessesMonitor.ViewModels;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    
+    public Person(string firstName , string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+}
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
+    
+    public ObservableCollection<Person> People { get; }
+    
+    public MainWindowViewModel()
+    {
+        var people = new List<Person> 
+        {
+            new Person("Neil", "Armstrong"),
+            new Person("Buzz", "Lightyear"),
+            new Person("James", "Kirk")
+        };
+        People = new ObservableCollection<Person>(people);
+    }
 }
