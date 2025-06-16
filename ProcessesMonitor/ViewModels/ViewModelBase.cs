7573e947cf9343084ad0180ff83954e1d6fb5f5a
Muhammad Ali <ma496@outlook.com>
2025-06-16 12:33:58 +0500

    init

---
+++ ProcessesMonitor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ProcessesMonitor.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ ProcessesMonitor/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ProcessesMonitor.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
